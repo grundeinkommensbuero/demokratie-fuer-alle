e89b8fe3b4fb2c7f44211a4d25c73c5076dcbdbe
nmauersberg <n.mauersberg@gmail.com>
2022-03-07 23:45:51 +0100

    Configure next and install dependencies

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  i18n: {
+    locales: ['de'],
+    defaultLocale: 'de',
+  },
+  images: {
+    dangerouslyAllowSVG: true,
+    domains: ['xbge-directus.frac.tools'],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
